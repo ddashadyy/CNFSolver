d4fa21fa1fd813e404d361086c1eb6bdbbf2dd81
ddashadyy <poletaevgena75@gmail.com>
2025-05-28 21:02:19 +0800

    selection function and cooling type

---
+++ include/Enums/AlgorithmTypes.hpp
@@ -0,0 +1,20 @@
+#pragma once 
+
+
+namespace utils
+{
+    
+enum class selection_function
+{
+    kRandom,
+    kLinear,
+    kExponential
+};
+
+enum class cooling_type
+{
+    kLinear,
+    kExponential
+};
+
+} // namespace utils
