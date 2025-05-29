73dddef880c316bb1266505db88ca5a3b675c124
ddashadyy <poletaevgena75@gmail.com>
2025-05-29 20:11:10 +0800

    int main

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <Algorithms/GeneticAlgorithm.hpp>
+
+#include <UI/MainWindow.hpp>
+#include <UI/GAWindow.hpp>
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include <windows.h>
+#include <commctrl.h> 
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow
+)
+{
+    (void)hPrevInstance;
+    (void)lpCmdLine;
+
+    try 
+    {
+        INITCOMMONCONTROLSEX icc = { sizeof(INITCOMMONCONTROLSEX) };
+        icc.dwICC = ICC_STANDARD_CLASSES | ICC_WIN95_CLASSES;
+        InitCommonControlsEx(&icc);
+
+        if (!ui::MainWindow::RegisterMWClass(hInstance)) 
+            throw std::runtime_error("Failed to register main window class");
+        
+        
+        if (!ui::MainWindow::CreateAndShow(hInstance, nCmdShow)) 
+            throw std::runtime_error("Failed to create main window");
+
+        MSG msg = {};
+        while (GetMessage(&msg, nullptr, 0, 0)) 
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        return static_cast<int>(msg.wParam);
+
+    } 
+    catch (const std::exception& e) 
+    {
+        MessageBoxA(nullptr, e.what(), "Error", MB_ICONERROR);
+        return 1;
+    } 
+    catch (...) 
+    {
+        MessageBoxW(nullptr, L"Unknown error occurred", L"Error", MB_ICONERROR);
+        return 1;
+    }
+}
+
+
+int main() 
+{
+    return wWinMain(
+        GetModuleHandle(nullptr),
+        nullptr,
+        GetCommandLineW(),
+        SW_SHOWDEFAULT
+    );
+}
+
